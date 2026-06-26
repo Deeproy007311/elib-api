70e8ba4ca02d41385b8e449a829eb5ad7b829cf2
Deep Roy <royd5333@gmail.com>
2026-06-26 16:16:06 +0530

    created book endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./books/bookRouter";
 
 const app = express();
 app.use(express.json());
@@ -8,11 +9,12 @@ app.use(express.json());
 // Routes
 app.get("/", (req, res) => {
   res.json({
-    message: "Hii"
+    message: "Hii",
   });
 });
 
-app.use("/api/users",userRouter);
+app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
---
+++ src/books/bookController.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+const createBook = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+
+    
+};
+
+export { createBook };
